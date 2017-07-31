2376b0f525f86b7f9bb7154a4c48cf199ca39499
Medicine Yeh <medicinehy@gmail.com>
2017-07-31 15:28:21 +0800

    [Update] Fixes of snippit_ui/scripts/parse.py and rewrite demo.sh

--- demo.sh
+++ demo.sh
@@ -3,21 +3,16 @@ SCRIPT_DIR=$(cd "$(dirname $0)" && pwd)
 COLOR_GREEN="\033[1;32m"
 NC="\033[0;00m"
 
-function run_demo() {
-    cd "$SCRIPT_DIR/qemu_image"
-    ./do_test.expect "./profile.sh --phase ./test_set/matrix" 1 "$SCRIPT_DIR/demo"
-    cd "$SCRIPT_DIR"
-    ./snippit_ui/scripts/parse.py -i ./demo/phase/* -c
-    cd "$SCRIPT_DIR/snippit_ui/public"
-    echo "Please open your browser and enter the following URL"
-    python3 -m http.server
-}
+# Auto-run
+cd "$SCRIPT_DIR/qemu_image"
+./do_test.expect "./profile.sh --phase ./test_set/matrix" 1 "$SCRIPT_DIR/demo"
 
-run_demo
-
-echo "It's now your turn"
-sleep 3
+# Parse phase of a process, speficy the PID
 cd "$SCRIPT_DIR"
-source "$SCRIPT_DIR/install_command.sh"
-snippit qemu vexpress
+./snippit_ui/scripts/parse.py -i ./demo/phase/* -c
+
+# Show in browser
+cd "$SCRIPT_DIR/snippit_ui/public"
+echo "Please open your browser and enter the following URL"
+python3 -m http.server
 
