304a00168eeeec691b60fc32078ec1b6d9055e50
Medicine Yeh <medicinehy@gmail.com>
2017-07-31 18:36:35 +0800

    Add command checks in demo.sh

--- demo.sh
+++ demo.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd "$(dirname $0)" && pwd)
+COLOR_RED='\033[1;31m'
 COLOR_GREEN="\033[1;32m"
 NC="\033[0;00m"
 
 # Auto-run
 cd "$SCRIPT_DIR/qemu_image"
 ./do_test.expect "./profile.sh --phase ./test_set/matrix" 1 "$SCRIPT_DIR/demo"
+[[ $? != 0 ]] && echo -e "${COLOR_RED}Something wrong happened in the expect script${NC}"
 
 # Parse phase of a process, speficy the PID
 cd "$SCRIPT_DIR"
